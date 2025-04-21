b8ea6d8a35522d64baea44ff30e75a31d8d69712
Madhes2316 <99384306+Madhes2316@users.noreply.github.com>
2025-04-21 20:42:25 +0530

    Create BasicPrograms-1.cs

---
+++ BasicPrograms-1.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1.First n Even numbers
+            //Console.Write("First n Even numbers?: ");
+            //int evenNumberCount = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= evenNumberCount; i++)
+            //{
+            //    Console.WriteLine(i * 2);
+            //}
+
+            //2.Factorial of a number
+            //Console.Write("First number to do factorial: ");
+            //int userNum = Convert.ToInt32(Console.ReadLine());
+            //int factorialNum = 1;
+
+            //if (userNum != 0)
+            //{
+            //    for (int i = 1; i <= userNum; i++)
+            //    {
+            //        factorialNum = factorialNum * i;
+            //    }
+            //    Console.WriteLine(factorialNum);
+            //}
+
+            //3.Basic Pattern
+            //Console.WriteLine("Enter pattern size");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= rows; i++) {
+
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //4.Reverse a number
+            //Console.WriteLine("Enter pattern size");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int revNumber = 0;
+
+            //while (number != 0) { 
+            //    int remainder = number % 10;
+            //    revNumber = (revNumber * 10) + remainder;
+            //    number = number / 10;
+            //}
+            //Console.WriteLine(revNumber);
+
+
+        }
+    }
+}
